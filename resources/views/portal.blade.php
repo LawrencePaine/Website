212fdd45794bb6fe2cf1cc442ed98726e1d58627
Lawrence Paine <l.paine@hotmail.co.uk>
2020-04-05 18:25:52 +0100

    First Commit

---
+++ resources/views/portal.blade.php
@@ -0,0 +1,59 @@
+<html class="no-js gr__lawrencepaine_eng" lang="en"><!--<![endif]--><head>
+@extends('layouts.app')
+
+@section('content')
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top" data-offset="66" data-gr-c-s-loaded="true" style="display: block;">
+    <header id="home" class="hero">
+        <div class="hero-body">
+            <div class="hero-text text-center">
+                <h1>
+                    Lawrence <strong>Paine</strong> <span class="blinker"></span>
+                </h1>
+
+                <h2 style="text-transform: uppercase; padding-top: 40px"><div><strong>Website</strong> Developer</div> <div style="padding-top: 10px">Certified <strong>Laravel</strong> Developer</div><div style="padding-top: 10px"><strong>PHP</strong> Developer</div></h2>
+
+                <p class="uppercase" style="padding-top: 10px">Remote / Glasgow, Scotland</p>
+
+                <div class="page-scroll">
+                    <a href="#profile" class="btn btn-lg btn-custom">
+                        Know Me Better
+                    </a>
+                </div>
+                <!-- // .page-scroll -->
+            </div>
+            <!-- // .hero-text -->
+        </div>
+        <!-- // .hero-body -->
+    </header>
+    <!-- NAVIGATION START -->
+    <div id="navigation-sticky-wrapper" class="sticky-wrapper" style="height: 66px;"><nav id="navigation" class="navbar navbar-fixed-top center-menu">
+        <div class="container navbar-container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".berg-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a class="navbar-brand visible-xs visible-sm" href="#page-top">Fraser Murray</a>
+            </div>
+            <!-- // .navbar-header -->
+
+            <div class="navbar-collapse collapse berg-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="page-scroll active"><a href="#home">Home</a></li>
+                    <li class="page-scroll"><a href="#profile">Profile</a></li>
+                    <li class="page-scroll"><a href="#portfolio">Portfolio</a></li>
+                    <li class="page-scroll"><a href="#services">Services</a></li>
+                    <li class="page-scroll"><a href="#resume">Resume</a></li>
+                    <li class="page-scroll"><a href="#references">References</a></li>
+                    <li class="page-scroll"><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+            <!-- // .navbar-collapse -->
+        </div>
+        <!-- // .navbar-container -->
+    </nav></div>
+    <!-- // NAVIGATION END -->
+</body><html
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+use Illuminate\Support\Facades\Auth;
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::get('/portal', 'portalController@index')->name('portal');
+
+Route::get('/contact', function() {
+    return view('contact');
+});
+
+Route::get('/portal', function() {
+   return view('portal');
+});
+// Route::get('/contact', 'ContactController@index')->name('contact');
